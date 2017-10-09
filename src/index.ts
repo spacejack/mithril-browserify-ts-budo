40f1d62b6814704a2d3b74c6f1d9ec8f46bce684
spacejack <spacejack@gmail.com>
2017-10-09 14:09:08 -0400

    Overhaul configuration, style

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 // App entry point
-import * as m from 'mithril'
-import home from './components/home'
-import about from './components/about'
+import * as m from 'mithril';
+import home from './components/home';
+import about from './components/about';
 
 m.route(document.body, '/', {
-	'/': home,
-	'/about': about
-})
+  '/': home,
+  '/about': about
+});
 
 ///////////////////////////////////////////////////////////
 // For browserify-hmr
 // See browserify-hmr module.hot API docs for hooks docs.
-declare const module: any // tslint:disable-line no-reserved-keywords
+declare const module: any; // tslint:disable-line no-reserved-keywords
 if (module.hot) {
-	module.hot.accept()
-	// module.hot.dispose((data: any) => {
-	// 	m.redraw()
-	// })
+  module.hot.accept();
+  // module.hot.dispose((data: any) => {
+  // 	m.redraw();
+  // })
 }
 ///////////////////////////////////////////////////////////
