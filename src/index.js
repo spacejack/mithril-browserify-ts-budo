a6857f3319c704ba0547ae4f4080105951d5a530
spacejack <spacejack@gmail.com>
2017-10-09 14:35:27 -0400

    Cleanup style

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // App entry point
-import m from 'mithril'
-import home from './components/home'
-import about from './components/about'
+import m from 'mithril';
+import home from './components/home';
+import about from './components/about';
 
 m.route(document.body, '/', {
-	'/': home,
-	'/about': about
-})
+  '/': home,
+  '/about': about
+});
 
 ///////////////////////////////////////////////////////////
 // For browserify-hmr
 // See browserify-hmr module.hot API docs for hooks docs.
 /*global module */
 if (module.hot) {
-	module.hot.accept()
+  module.hot.accept();
 }
 ///////////////////////////////////////////////////////////
